75a8236fd0fee708d0f5c639581134d9382e9de6
Hanxbeen <hanxbeen@gmail.com>
2021-08-19 10:19:16 +0900

    study : 'JSX 내부, 즉 함수형 컴포넌트 안에서 변수를 보여줄때는 {}'

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Hello from "./Hello";
 
 function App() {
+  const name = "Hanxbeen";
   return (
     <>
       <Hello />
       <div>
-        <h2>안녕히 계세요..</h2>
+        <h1>저는 {name} 입니다</h1>
+        <h2>그럼 20000, 안녕히 계세요..</h2>
       </div>
     </>
   );
