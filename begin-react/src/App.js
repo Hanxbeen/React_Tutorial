7134d68277aca343a7bfb791e9e5a2c52d9f079f
Hanxbeen <hanxbeen@gmail.com>
2021-08-19 11:21:35 +0900

    study : 'JSX에서 style과 CSS class설정 방법'

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -3,10 +3,16 @@ import Hello from "./Hello";
 
 function App() {
   const name = "Hanxbeen";
+  const style = {
+    backgroundColor: "Black",
+    color: "aqua",
+    fontSize: 24,
+    padding: "1rem",
+  };
   return (
     <>
       <Hello />
-      <div>
+      <div style={style}>
         <h1>저는 {name} 입니다</h1>
         <h2>그럼 20000, 안녕히 계세요..</h2>
       </div>
