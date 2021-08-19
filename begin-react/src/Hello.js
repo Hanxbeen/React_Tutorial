13c26ab58f86248b2a14fcfc26f417855e1939e0
Hanxbeen <hanxbeen@gmail.com>
2021-08-19 11:28:29 +0900

    study : 'App.css에서 className으로 태그와 스타일 연동'

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./App.css";
 
 function Hello() {
   const name = "Hanxbeen";
@@ -14,6 +14,9 @@ function Hello() {
     <div style={style}>
       <h1>저는 {name} 입니다</h1>
       <h2>그럼 20000, 안녕히 계세요..</h2>
+      <div className="gray-box"></div>
+      <div className="gray-box"></div>
+      <div className="gray-box"></div>
     </div>
   );
 }
