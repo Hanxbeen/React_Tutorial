2202f04a7351f2b665413ffea29ec03a224ba729
Hanxbeen <hanxbeen@gmail.com>
2021-08-19 15:30:53 +0900

    study : '컴포넌트 태그 사이에 들어간 값을 조회하는 법'

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Hello from "./Hello";
+import Wrapper from "./Wrapper";
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <Hello name="props를 통해 컴포넌트에게 값 전달" color="red" />;
       <Hello color="pink" />
-    </>
+    </Wrapper>
   );
 }
 
