6ca0f30b0bd95cddcb825f22db2de57b5fa0d745
Hanxbeen <hanxbeen@gmail.com>
2021-08-19 11:24:25 +0900

    study : '함수형 컴포넌트 불러오기'

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -2,22 +2,7 @@ import React from "react";
 import Hello from "./Hello";
 
 function App() {
-  const name = "Hanxbeen";
-  const style = {
-    backgroundColor: "Black",
-    color: "aqua",
-    fontSize: 24,
-    padding: "1rem",
-  };
-  return (
-    <>
-      <Hello />
-      <div style={style}>
-        <h1>저는 {name} 입니다</h1>
-        <h2>그럼 20000, 안녕히 계세요..</h2>
-      </div>
-    </>
-  );
+  return <Hello />;
 }
 
 export default App;
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+
 function Hello() {
-  return <div>안녕하세요</div>;
+  const name = "Hanxbeen";
+
+  const style = {
+    backgroundColor: "Black",
+    color: "aqua",
+    fontSize: 24,
+    padding: "1rem",
+  };
+  return (
+    <div style={style}>
+      <h1>저는 {name} 입니다</h1>
+      <h2>그럼 20000, 안녕히 계세요..</h2>
+    </div>
+  );
 }
 
 export default Hello;
