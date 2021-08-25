b9117b64c140e237ae78413e2d8fa56fbabae8c3
Hanxbeen <hanxbeen@gmail.com>
2021-08-25 16:43:34 +0900

    feat : '회원 직무성격에 따라  다른 데이터 출력'

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profile from "./Profile";
 
 const App = () => {
   return (
@@ -17,10 +18,21 @@ const App = () => {
             <button>메인</button>
           </Link>
         </li>
+        <li>
+          <Link to="/profiles/admin">
+            <button>관리자 로그인</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/profiles/laborer">
+            <button>일용직 로그인</button>
+          </Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/profiles/:username" component={Profile} />
     </div>
   );
 };
--- router-tutorial/src/Profile.js
+++ router-tutorial/src/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// 프로필에서 사용할 데이터
 const profileData = {
   admin: {
     name: "인사담당자",
@@ -9,4 +10,27 @@ const profileData = {
     name: "일용직 근로자",
     description: "공장 일용 근로자",
   },
+//   undefined: {
+//     name: "No one",
+//     description: "no one",
+//   },
 };
+
+// 파라미터를 받아올 땐 match 안에 들어있는 params 값을 참조할 예정
+const Profile = ({ match }) => {
+  const { username } = match.params;
+  const profile = profileData[username];
+  if (!profile) {
+    return <div>존재하지 않는 유저입니다.</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {username}({profile.name})
+      </h3>
+      <p>{profile.description}</p>
+    </div>
+  );
+};
+
+export default Profile;
