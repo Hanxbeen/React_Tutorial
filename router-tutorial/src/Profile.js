22f9b9ed25c78da51b264ff240a7f2501eb5babc
Hanxbeen <hanxbeen@gmail.com>
2021-08-25 16:10:27 +0900

    feat : 'profile 파라미터';

---
+++ router-tutorial/src/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const profileData = {
+  admin: {
+    name: "인사담당자",
+    description: "ERP 전반에 대한 관리 책임자",
+  },
+  laborer: {
+    name: "일용직 근로자",
+    description: "공장 일용 근로자",
+  },
+};
