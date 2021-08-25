cf00bcff52009569b2516624d53f48653cc1ffe2
Hanxbeen <hanxbeen@gmail.com>
2021-08-25 17:01:38 +0900

    feat : '페이지에서 쿼리를 받아오기 '

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import qs from "qs";
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const detail = query.admin === "true"; // 쿼리의 파싱결과값은 문자열입니다.
   return (
     <div>
       <h1>메인</h1>
       <p>리액트 라우터로 메인페이지 이동 테스트.</p>
+      {detail && (
+        <p>관리자 로그인 시에만 보여줄 정보들.. 어쩌고 저쩌고 룰루랄랄</p>
+      )}
     </div>
   );
 };
