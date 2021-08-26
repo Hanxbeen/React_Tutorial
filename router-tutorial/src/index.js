9b8a8d640015214ccfa54a28b93376fb1e1ccc57
Hanxbeen <hanxbeen@gmail.com>
2021-08-26 15:36:16 +0900

    feat : 'IE11에서 호환 polyfill 설치 및 설정'

--- router-tutorial/src/index.js
+++ router-tutorial/src/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // * BrowserRouter 불러오기
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; // * BrowserRouter 불러오기
+import "./index.css";
+import App from "./App";
 //import * as serviceWorker from './serviceWorker';
+
+// IE9의 경우
+// import 'react-app-polyfill/ie9';
+// import 'react-app-polyfill/stable';
+
+// IE11의 경우
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 
 // * App 을 BrowserRouter 로 감싸기
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 //serviceWorker.unregister();
