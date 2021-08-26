424c2e34ed5af0bfd1a931e760a5cfb91ed25adf
Hanxbeen <hanxbeen@gmail.com>
2021-08-26 16:41:32 +0900

    feat : '존재하지 않는 페이지에 대한 설정 '

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 // import Profile from "./Profile";
@@ -49,11 +49,22 @@ const App = () => {
         </li> */}
       </ul>
       <hr />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/profiles" component={Profiles} />{" "}
-      <Route path="/history" component={HistorySample} />
-      {/* <Route path="/profiles/:username" component={Profile} /> */}
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/profiles" component={Profiles} />{" "}
+        <Route path="/history" component={HistorySample} />
+        {/* <Route path="/profiles/:username" component={Profile} /> */}{" "}
+        <Route
+          // path 를 따로 정의하지 않으면 모든 상황에 렌더링됨
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
