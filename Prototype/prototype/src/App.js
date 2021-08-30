f2cabb1f50420b21f69287fb39a8129ede4a1174
Hanxbeen <hanxbeen@gmail.com>
2021-08-30 16:47:56 +0900

    feat : '프로토타입 메인 페이지 및 로그인 페이지 생성'

--- Prototype/prototype/src/App.js
+++ Prototype/prototype/src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
-import About from "./About";
-import Home from "./Home";
-// import Profile from "./Profile";
-import Profiles from "./Profiles";
-import HistorySample from "./HistorySample";
+// import About from "./About";
+import StartPage from "./StartPage";
 
 const App = () => {
   // let userCheck = "admin";
@@ -17,7 +14,7 @@ const App = () => {
             <button>홈</button>
           </Link>
         </li>
-
+        {/*
         <li>
           <Link to="/about?admin=true">
             <button>관리자로 로그인 한다면?</button>
@@ -36,7 +33,7 @@ const App = () => {
         <li>
           <Link to="/history">뒤로가기나 홈버튼 구현</Link>
         </li>
-        {/*<li>
+        <li>
           <Link to="/profiles/admin">
             <button>관리자 페이지</button>
           </Link>
@@ -49,11 +46,9 @@ const App = () => {
       </ul>
       <hr />
       <Switch>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/profiles" component={Profiles} />{" "}
-        <Route path="/history" component={HistorySample} />
-        {/* <Route path="/profiles/:username" component={Profile} /> */}{" "}
+        <Route path="/" exact={true} component={StartPage} />
+        {/* <Route path="/about" component={About} /> */}
+        {/* <Route path="/profiles/:username" component={Profile} /> */}
         <Route
           // path 를 따로 정의하지 않으면 모든 상황에 렌더링됨
           render={({ location }) => (
--- Prototype/prototype/src/index.js
+++ Prototype/prototype/src/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; // * BrowserRouter 불러오기
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dis/css/bootstrap.min.css";
+import 'bootstrap/dis/css/bootstrap.min.css';
+//import * as serviceWorker from './serviceWorker';
 
+// IE9의 경우
+// import 'react-app-polyfill/ie9';
+// import 'react-app-polyfill/stable';
+
+// IE11의 경우
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+
+// * App 을 BrowserRouter 로 감싸기
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//serviceWorker.unregister();
