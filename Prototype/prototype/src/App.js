781723e586159ae39fc64f6b8cd3ba0b1841b756
Hanxbeen <hanxbeen@gmail.com>
2021-08-30 14:35:38 +0900

    chore : 'react strap 설치 및 환경설정'

--- Prototype/prototype/src/App.js
+++ Prototype/prototype/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+// import Profile from "./Profile";
+import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
-function App() {
+const App = () => {
+  // let userCheck = "admin";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">
+            <button>홈</button>
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/about?admin=true">
+            <button>관리자로 로그인 한다면?</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/about?laborer=true">
+            <button>일반 사용자로 로그인 한다면?</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/profiles">
+            <button>프로필 목록</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/history">뒤로가기나 홈버튼 구현</Link>
+        </li>
+        {/*<li>
+          <Link to="/profiles/admin">
+            <button>관리자 페이지</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/profiles/laborer">
+            <button>일반 사용자 페이지</button>
+          </Link>
+        </li> */}
+      </ul>
+      <hr />
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/profiles" component={Profiles} />{" "}
+        <Route path="/history" component={HistorySample} />
+        {/* <Route path="/profiles/:username" component={Profile} /> */}{" "}
+        <Route
+          // path 를 따로 정의하지 않으면 모든 상황에 렌더링됨
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
