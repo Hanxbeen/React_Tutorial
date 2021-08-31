bc9a3f30633aae17966ab8b3f0162cef49bc1588
Hanxbeen <hanxbeen@gmail.com>
2021-08-31 14:26:22 +0900

    feat : '로그아웃 확인 및 취소 상태 분류 성공'

--- Prototype/prototype/src/MainPage.js
+++ Prototype/prototype/src/MainPage.js
@@ -1,29 +1,13 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { Button } from "react-bootstrap";
-import { useState } from "react";
 
 const MainPage = () => {
-  const [state, setState] = useState(0);
-  const useConfirm = (message = null, onConfirm, onCancel) => {
-    const confirmAction = () => {
-      if (window.confirm(message)) {
-        onConfirm();
-        setState(0);
-      } else {
-        onCancel();
-        setState(1);
-      }
-    };
-
-    return confirmAction;
-  };
   const history = useHistory();
 
-  const routeChange = () => {
-    confirmLogout();
-    console.log(state);
-    if (state) {
+  const logout = () => {
+    let check = window.confirm("로그아웃 하시겠습니까?");
+    if (check) {
       let path = `/`;
       history.push(path);
     } else {
@@ -31,21 +15,14 @@ const MainPage = () => {
       history.push(path);
     }
   };
-  const moveConfirm = () => console.log("로그아웃 했습니다.");
-  const cancelConfirm = () => console.log("취소했습니다.");
-  const confirmLogout = useConfirm(
-    "로그아웃 하시겠습니까?",
-    moveConfirm,
-    cancelConfirm
-  );
   return (
     <div>
-      <h1>이곳은 메인</h1>
-      <p>모든 메뉴들이 있찌</p>
+      <h1>메인</h1>
+      <p>모든 메뉴들</p>
       {/* <Link to="/">
         <Button onClick={confirmLogout}>홈</Button>
       </Link> */}
-      <Button onClick={routeChange}>로그아웃</Button>
+      <Button onClick={logout}>로그아웃</Button>
     </div>
   );
 };
