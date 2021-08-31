80067311b7e30adec5ff3f5ebcff9891c30b296c
Hanxbeen <hanxbeen@gmail.com>
2021-08-31 09:09:57 +0900

    chore : 'reactstrap 설치 및 환경설정'

--- Prototype/prototype/src/index.js
+++ Prototype/prototype/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom"; // * BrowserRouter 불러오기
 import "./index.css";
 import App from "./App";
-import 'bootstrap/dis/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.css";
 //import * as serviceWorker from './serviceWorker';
 
 // IE9의 경우
