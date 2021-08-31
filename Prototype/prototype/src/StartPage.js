718204df1acd5ed13fa3a5323dd37d9d65bd5b43
Hanxbeen <hanxbeen@gmail.com>
2021-08-31 16:57:38 +0900

    feat : 'GUI와 맞게 css 수정 및 마무리 '

--- Prototype/prototype/src/StartPage.js
+++ Prototype/prototype/src/StartPage.js
@@ -22,26 +22,52 @@ const StartPage = () => {
       >
         <Form.Group className="companyCode" controlId="companyCode">
           <Form.Label>회사코드</Form.Label>
-          <Form.Control type="text" placeholder="회사코드" />
-          <Form.Text className="text-muted">회사 코드를 찾아주세요</Form.Text>
+          <div style={{ display: "flex", height: "40px" }}>
+            <Form.Control type="text" placeholder="회사코드" />
+            <Button
+              variant="dark"
+              type="search"
+              style={{ marginLeft: "5px", width: "100px", fontSize: "13px" }}
+            >
+              검색
+            </Button>
+          </div>
+          {/*<Form.Text className="text-muted">회사 코드를 찾아주세요</Form.Text>*/}
         </Form.Group>
+
         <Form.Group className="companyNumber" controlId="companyNumber">
           <Form.Label>사번</Form.Label>
-          <Form.Control type="number" placeholder="사번" />
-          <Form.Text className="text-muted">사번을 입력해주세요</Form.Text>
+          <div style={{ display: "flex", height: "40px" }}>
+            <Form.Control type="text" placeholder="사번" />
+            {/* <Form.Text className="text-muted">사번을 입력해주세요</Form.Text>*/}
+            <Button
+              variant="dark"
+              type="submit"
+              style={{ marginLeft: "5px", width: "100px", fontSize: "13px" }}
+            >
+              PW찾기
+            </Button>
+          </div>
         </Form.Group>
+
         <Form.Group className="password" controlId="password">
           <Form.Label>비밀번호</Form.Label>
-          <Form.Control type="password" placeholder="비밀번호" />
+          <div style={{ display: "flex", height: "40px" }}>
+            <Form.Control type="password" placeholder="비밀번호" />
+            <div
+              style={{
+                marginLeft: "5px",
+                width: "100px",
+              }}
+            >
+              <Link to="/main">
+                <Button style={{ height: "40px", fontSize: "13px" }}>
+                  로그인
+                </Button>
+              </Link>
+            </div>
+          </div>
         </Form.Group>
-        <div style={{ margin: 10 }}>
-          <Button variant="danger" type="submit">
-            PW찾기
-          </Button>
-          <Link to="/main">
-            <Button>로그인</Button>
-          </Link>
-        </div>
       </Form>
     </div>
   );
