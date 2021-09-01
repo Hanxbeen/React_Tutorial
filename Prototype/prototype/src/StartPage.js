a02570e49842d594e01386204a9ddb32fd8d2da7
Hanxbeen <hanxbeen@gmail.com>
2021-09-01 11:23:51 +0900

    feat : '회사코드 선택 시 state.name에 데이터 담기'

--- Prototype/prototype/src/StartPage.js
+++ Prototype/prototype/src/StartPage.js
@@ -6,90 +6,80 @@ import CompanyCodeModal from "./modal/companyCode";
 
 function StartPage() {
   const [modalShow, setModalShow] = useState(false);
-
+  const [checkedName, setCheckedName] = useState('');
   return (
-    <div>
-      <Button
-        variant="dark"
-        type="search"
-        style={{ marginLeft: "5px", width: "100px", fontSize: "13px" }}
-        onClick={() => setModalShow(true)}
-      >
-        검색
-      </Button>
-      <div
-        //modal이 작동이 안돼 form 태그를 div로 바꿈.. 폼태그에서 모달이 실행이 안되는이유가 뭘까?
-        style={{
-          //display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          width: "300px",
-          height: "400px",
-          marginLeft: "-150px",
-          marginTop: "-400px",
-        }}
-      >
-        <Image
-          src="img/KETI_LOGO.jpg"
-          rounded
-          style={{ width: "300px", height: "350px" }}
-        />
-        <Form.Group className="companyCode" controlId="companyCode">
-          <Form.Label>회사코드</Form.Label>
-          <div style={{ display: "flex", height: "40px" }}>
-            <Form.Control type="text" placeholder="회사코드" />
-            <Button
-              variant="dark"
-              type="search"
-              style={{ marginLeft: "5px", width: "100px", fontSize: "13px" }}
-              onClick={() => setModalShow(true)}
-            >
-              검색
-            </Button>
-            <CompanyCodeModal
-              show={modalShow}
-              onHide={() => setModalShow(false)}
-            />
-          </div>
-          {/*<Form.Text className="text-muted">회사 코드를 찾아주세요</Form.Text>*/}
-        </Form.Group>
-        <Form.Group className="companyNumber" controlId="companyNumber">
-          <Form.Label>사번</Form.Label>
-          <div style={{ display: "flex", height: "40px" }}>
-            <Form.Control type="text" placeholder="사번" />
-            {/* <Form.Text className="text-muted">사번을 입력해주세요</Form.Text>*/}
-            <Button
-              variant="dark"
-              type="submit"
-              style={{ marginLeft: "5px", width: "100px", fontSize: "13px" }}
-            >
-              PW찾기
-            </Button>
-          </div>
-        </Form.Group>
-        <Form.Group className="password" controlId="password">
-          <Form.Label>비밀번호</Form.Label>
-          <div style={{ display: "flex", height: "40px" }}>
-            <Form.Control type="password" placeholder="비밀번호" />
+    <div
+      //modal이 작동이 안돼 form 태그를 div로 바꿈.. 폼태그에서 모달이 실행이 안되는이유가 뭘까?
+      style={{
+        //display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        width: "300px",
+        height: "400px",
+        marginLeft: "-150px",
+        marginTop: "-430px",
+      }}
+    >
+      <Image
+        src="img/KETI_LOGO.jpg"
+        rounded
+        style={{ width: "300px", height: "350px" }}
+      />
+      <Form.Group className="companyCode" controlId="companyCode">
+        <Form.Label>회사코드</Form.Label>
+        <div style={{ display: "flex", height: "40px" }}>
+          <Form.Control type="text" placeholder="회사코드" value={checkedName} />
+          <Button
+            variant="dark"
+            type="search"
+            style={{ marginLeft: "5px", width: "100px", fontSize: "13px" }}
+            onClick={() => setModalShow(true)}
+          >
+            검색
+          </Button>
+          <CompanyCodeModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+        </div>
+        {/*<Form.Text className="text-muted">회사 코드를 찾아주세요</Form.Text>*/}
+      </Form.Group>
+      <Form.Group className="companyNumber" controlId="companyNumber">
+        <Form.Label>사번</Form.Label>
+        <div style={{ display: "flex", height: "40px" }}>
+          <Form.Control type="text" placeholder="사번" />
+          {/* <Form.Text className="text-muted">사번을 입력해주세요</Form.Text>*/}
+          <Button
+            variant="dark"
+            type="submit"
+            style={{ marginLeft: "5px", width: "100px", fontSize: "13px" }}
+          >
+            PW찾기
+          </Button>
+        </div>
+      </Form.Group>
+      <Form.Group className="password" controlId="password">
+        <Form.Label>비밀번호</Form.Label>
+        <div style={{ display: "flex", height: "40px" }}>
+          <Form.Control type="password" placeholder="비밀번호" />
 
-            <Link to="/main">
-              <Button
-                style={{
-                  height: "40px",
-                  fontSize: "13px",
-                  marginLeft: "5px",
-                  width: "78px",
-                }}
-              >
-                로그인
-              </Button>
-            </Link>
-          </div>
-        </Form.Group>
-      </div>
+          <Link to="/main">
+            <Button
+              style={{
+                height: "40px",
+                fontSize: "13px",
+                marginLeft: "5px",
+                width: "78px",
+              }}
+            >
+              로그인
+            </Button>
+          </Link>
+        </div>
+      </Form.Group>
     </div>
   );
 }
--- Prototype/prototype/src/modal/companyCode.js
+++ Prototype/prototype/src/modal/companyCode.js
@@ -1,40 +1,116 @@
-import React from "react";
+import { React } from "react";
 import { Button, Modal, Container, Row, Col } from "react-bootstrap";
 
 function companyCode(props) {
+  let state = {
+    name: "",
+  };
+  function sendCompanyName(e) {
+    state.name = e.target.innerText;
+    console.log(state.name);
+  }
   return (
-    <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Using Grid in Modal
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">회사코드</Modal.Title>
       </Modal.Header>
       <Modal.Body className="show-grid">
         <Container>
-          <Row>
-            <Col xs={12} md={8}>
-              .col-xs-12 .col-md-8
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
+          <Row
+            style={{
+              backgroundColor: "black",
+              color: "white",
+              textAlign: "center",
+              fontSize: "17px",
+              height: "40px",
+              alignItems: "center",
+            }}
+          >
+            <Col>회사명</Col>
+            <Col>회사코드</Col>
+          </Row>
+          <Row
+            style={{
+              backgroundColor: "#D3D3D3",
+              textAlign: "center",
+              color: "black",
+              height: "50px",
+              alignItems: "center",
+            }}
+          >
+            <Col>A</Col>
+            <Col>
+              <Button
+                variant="outline-light"
+                style={{ border: "0px", width: "100%" }}
+                onClick={sendCompanyName}
+              >
+                100000
+              </Button>
             </Col>
           </Row>
-
-          <Row>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
+          <Row
+            style={{
+              backgroundColor: "#DCDCDC",
+              textAlign: "center",
+              color: "black",
+              height: "50px",
+              alignItems: "center",
+            }}
+          >
+            <Col>B</Col>
+            <Col>
+              <Button
+                variant="outline-secondary"
+                style={{ border: "0px", width: "100%" }}
+                onClick={sendCompanyName}
+              >
+                200000
+              </Button>
             </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
+          </Row>
+          <Row
+            style={{
+              backgroundColor: "#D3D3D3",
+              textAlign: "center",
+              color: "black",
+              height: "50px",
+              alignItems: "center",
+            }}
+          >
+            <Col>C</Col>
+            <Col>
+              <Button
+                variant="outline-dark"
+                style={{ border: "0px", width: "100%" }}
+                onClick={sendCompanyName}
+              >
+                300000
+              </Button>
             </Col>
           </Row>
         </Container>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        <Row style={{ width: "100%" }}>
+          <Col md="9">
+            <p
+              style={{ color: "gray", fontSize: "13px" }}
+              onChange={state.name}
+            >
+              회사 코드를 선택해주세요
+            </p>
+          </Col>
+          <Col md="1">
+            <Button
+              variant="dark"
+              onClick={props.onHide}
+              style={{ width: "100px" }}
+            >
+              선택
+            </Button>
+          </Col>
+        </Row>
       </Modal.Footer>
     </Modal>
   );
