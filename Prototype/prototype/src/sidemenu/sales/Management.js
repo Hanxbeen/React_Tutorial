144f96983223aaa13b6439d635611e4285157a96
Hanxbeen <hanxbeen@gmail.com>
2021-09-01 16:49:35 +0900

    feat : '메인페이지 sidebar 만들기'

---
+++ Prototype/prototype/src/sidemenu/sales/Management.js
@@ -0,0 +1,12 @@
+import { React } from "react";
+
+function Management() {
+
+  return (
+   <div>
+     영업 관리페이지
+    </div>
+  );
+}
+
+export default Management;
---
+++ Prototype/prototype/src/sidemenu/sales/Status.js
@@ -0,0 +1,12 @@
+import { React } from "react";
+
+function Status() {
+
+  return (
+   <div>
+     영업 현황 조회 페이지
+    </div>
+  );
+}
+
+export default Status;
