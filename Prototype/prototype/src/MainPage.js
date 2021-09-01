8a7ab1f23afe1633e8b072f68d99481a7dccdc29
Hanxbeen <hanxbeen@gmail.com>
2021-09-01 16:50:02 +0900

    feat : '메인페이지 Nav bar 만들기'

--- Prototype/prototype/src/MainPage.js
+++ Prototype/prototype/src/MainPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, Navbar, Nav, Container, Tab, Row, Col } from "react-bootstrap";
+import salesStatus from "./sidemenu/sales/Status";
+import salesManagement from "./sidemenu/sales/Management";
 
 const MainPage = () => {
   const history = useHistory();
@@ -17,12 +19,49 @@ const MainPage = () => {
   };
   return (
     <div>
-      <h1>메인</h1>
-      <p>모든 메뉴들</p>
-      {/* <Link to="/">
-        <Button onClick={confirmLogout}>홈</Button>
-      </Link> */}
-      <Button onClick={logout}>로그아웃</Button>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ERP</Navbar.Brand>
+          <Nav className="me-auto">
+            {/* <Nav.Link href="#home">접속 상태</Nav.Link> */}
+            {/* <Nav.Link href="#features">사용자 정보</Nav.Link> */}
+            <Nav.Link href="#pricing">도움말(F1)</Nav.Link>
+            <Nav.Link href="#pricing">환경 설정</Nav.Link>
+            <Nav.Link href="#pricing">등록</Nav.Link>
+            <Nav.Link href="#pricing">삭제</Nav.Link>
+            <Nav.Link href="#pricing">저장</Nav.Link>
+            <Nav.Link href="#pricing">조회</Nav.Link>
+            <Nav.Link href="#pricing">출력</Nav.Link>
+            <Button variant="danger " onClick={logout}>
+              LOGOUT
+            </Button>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+        <Row>
+          <Col sm={3}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="first">영업관리</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">영업현황</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="first">
+                <salesStatus />
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">
+                <salesManagement />
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     </div>
   );
 };
