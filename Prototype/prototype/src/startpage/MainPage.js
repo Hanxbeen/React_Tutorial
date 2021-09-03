a4ac1a11f427785c421ca05191b2d2a6cfc15912
Hanxbeen <hanxbeen@gmail.com>
2021-09-03 16:43:37 +0900

    feat : '사이드바 오른쪽 뷰 설정'

--- Prototype/prototype/src/startpage/MainPage.js
+++ Prototype/prototype/src/startpage/MainPage.js
@@ -1,24 +1,32 @@
 import React from "react";
 // import { useHistory, Link } from "react-router-dom";
-// import {
-//   Image,
+import //   Image,
 //   Button,
 //   Navbar,
 //   Nav,
 //   // Container,
 //   NavDropdown,
-// } from "react-bootstrap";
+"react-bootstrap";
 import "./Start.css";
-import Sidebar from "./Sidebar";
-import Navibar from "./Navibar";
+import Sidebar from "../components/Sidebar";
+import Navibar from "../components/Navibar";
 const MainPage = () => {
   return (
     <>
       <div>
         <Navibar />
       </div>
-      <div>
+      {/* <div className="parent">
+        <Sidebar className="child" />
+        <p className="child">
+          ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+        </p>
+      </div> */}
+      <div style={{ display: "flex" }}>
         <Sidebar />
+        <p style={{ flex: 7, marginTop: "6vh" }}>
+          ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+        </p>
       </div>
     </>
   );
