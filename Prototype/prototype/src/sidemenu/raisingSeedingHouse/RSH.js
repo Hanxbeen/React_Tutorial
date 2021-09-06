0d5e7d2fb62bccbd3edc7787ffa103e271a82a44
Hanxbeen <hanxbeen@gmail.com>
2021-09-06 16:57:23 +0900

    feat : '육묘장 정보 리스트 및 수정 폼 생성'

--- Prototype/prototype/src/sidemenu/raisingSeedingHouse/RSH.js
+++ Prototype/prototype/src/sidemenu/raisingSeedingHouse/RSH.js
@@ -1,6 +1,6 @@
 import { React } from "react";
 import "./RSH.css";
-import { Row, Col, Card, Pagination } from "react-bootstrap";
+import { Row, Col, Card, Pagination, Form, Button } from "react-bootstrap";
 
 import Sidebar from "../../components/Sidebar";
 import Navibar from "../../components/Navibar";
@@ -12,92 +12,181 @@ function RSH() {
       </div>
       <div className="parent">
         <Sidebar className="child" />
-        {/* <p style={{ flex: 7, marginTop: "6vh" }}>육묘장</p> */}
-        <Row
+        <div
           style={{
-            flex: 6,
             marginTop: "7vh",
             marginLeft: "10px",
             marginRight: "10px",
+            width: "100%",
           }}
         >
-          <Col>
-            <Card small className="mb-4">
-              <Card.Body className="p-0 pb-3">
-                <table className="table mb-0">
-                  <thead className="bg-light">
-                    <tr>
-                      <th scope="col" className="border-0">
-                        #
-                      </th>
-                      <th scope="col" className="border-0">
-                        회사코드
-                      </th>
-                      <th scope="col" className="border-0">
-                        육묘장 명
-                      </th>
-                      <th scope="col" className="border-0">
-                        시/군 구/동
-                      </th>
-                      <th scope="col" className="border-0">
-                        도
-                      </th>
-                      <th scope="col" className="border-0">
-                        번호
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>100000</td>
-                      <td>화순 육묘장</td>
-                      <td>화순군 도곡면 쌍곡리 828-5</td>
-                      <td>전라남도</td>
-                      <td>061-371-5231</td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>200000</td>
-                      <td>GCL Farm</td>
-                      <td>화성시 장안면 석포리 718-54</td>
-                      <td>경기도</td>
-                      <td>031-351-8864</td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td>300000</td>
-                      <td>전주 육묘장</td>
-                      <td>전주시 00구 00</td>
-                      <td>전라북도</td>
-                      <td>063-000-0000</td>
-                    </tr>
-                    <tr>
-                      <td>4</td>
-                      <td>400000</td>
-                      <td>부안 육묘장</td>
-                      <td>부안군 00면 00</td>
-                      <td>전라북도</td>
-                      <td>063-000-0000</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Pagination style={{ alignSelf: "center", justifyContent: "center" }}>
+          <Row>
+            <Col>
+              <Card small className="mb-4">
+                <Card.Body className="p-0 pb-3">
+                  <table className="table mb-0">
+                    <thead className="bg-light">
+                      <tr>
+                        <th scope="col" className="border-0">
+                          #
+                        </th>
+                        <th scope="col" className="border-0">
+                          회사코드
+                        </th>
+                        <th scope="col" className="border-0">
+                          육묘장 명
+                        </th>
+                        <th scope="col" className="border-0">
+                          시/군 구/동
+                        </th>
+
+                        <th scope="col" className="border-0">
+                          번호
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>100000</td>
+                        <td>00 육묘장</td>
+                        <td>000도 00시 00구 00동 000</td>
+                        <td>063-000-0000</td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>100000</td>
+                        <td>00 육묘장</td>
+                        <td>000도 00시 00구 00동 000</td>
+                        <td>063-000-0000</td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td>100000</td>
+                        <td>00 육묘장</td>
+                        <td>000도 00시 00구 00동 000</td>
+                        <td>063-000-0000</td>
+                      </tr>
+                      <tr>
+                        <td>4</td>
+                        <td>100000</td>
+                        <td>00 육묘장</td>
+                        <td>000도 00시 00구 00동 000</td>
+                        <td>063-000-0000</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Pagination style={{ justifyContent: "center" }}>
             <Pagination.First />
             <Pagination.Prev />
             <Pagination.Item active>{1}</Pagination.Item>
             <Pagination.Item>{2}</Pagination.Item>
             <Pagination.Item>{3}</Pagination.Item>
             <Pagination.Item>{4}</Pagination.Item>
-            <Pagination.Ellipsis />
-            <Pagination.Item>{11}</Pagination.Item>
+            {/* <Pagination.Ellipsis />
+              <Pagination.Item>{11}</Pagination.Item> */}
             <Pagination.Next />
             <Pagination.Last />
           </Pagination>
-        </Row>
+          <hr></hr>
+          <Form
+            style={{
+              alignSelf: "center",
+              justifyContent: "center",
+              width: "100%",
+              padding: "200px",
+            }}
+          >
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>회사코드</Form.Label>
+                <div style={{ display: "flex", height: "40px" }}>
+                  <Form.Control
+                    type="text"
+                    placeholder="000000"
+                    // value={companyCode}
+                    readOnly
+                  />
+
+                  <Button
+                    variant="dark"
+                    type="search"
+                    style={{
+                      marginLeft: "5px",
+                      width: "100px",
+                      fontSize: "13px",
+                    }}
+                    // onClick={() => setModalShow(true)}
+                  >
+                    검색
+                  </Button>
+
+                  {/* <CompanyCodeModal
+                    show={modalShow}
+                    onHide={() => choice()}
+                    getdata={setCompanyCode}
+                  /> */}
+                </div>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>명칭</Form.Label>
+                <Form.Control type="password" placeholder="농장 이름" />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>주소</Form.Label>
+                <Form.Control placeholder="전라북도 전주시 덕진구 반룡로 111" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>전화번호</Form.Label>
+                <Form.Control type="password" placeholder="000-0000-0000" />
+              </Form.Group>
+            </Row>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>...</Form.Label>
+                <Form.Control />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>...</Form.Label>
+                <Form.Select defaultValue="Choose...">
+                  <option>...</option>
+                  <option>...</option>
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridZip">
+                <Form.Label>...</Form.Label>
+                <Form.Control />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" id="formGridCheckbox">
+              <Form.Check type="checkbox" label="상태 값" />
+            </Form.Group>
+
+            <Button
+              variant="dark"
+              type="submit"
+              style={{
+                marginTop: "20px",
+                alignSelf: "center",
+                justifyContent: "center",
+              }}
+            >
+              수정하기
+            </Button>
+          </Form>
+        </div>
       </div>
     </>
   );
