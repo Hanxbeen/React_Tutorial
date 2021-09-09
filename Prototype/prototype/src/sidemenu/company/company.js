a2f60cb4a48afe0c0aebed6bc5e3026dd29a1ab5
Hanxbeen <hanxbeen@gmail.com>
2021-09-09 10:47:18 +0900

    feat : '회사관리 페이지 flex 틀 맞추기'

--- Prototype/prototype/src/sidemenu/company/company.js
+++ Prototype/prototype/src/sidemenu/company/company.js
@@ -1,19 +1,49 @@
 import { React } from "react";
 import "./Company.css";
+// import { Row, Col, Form, Button } from "react-bootstrap";
+// import TableCustom from "../../components/TableCustom";
 
 import Sidebar from "../../components/Sidebar";
 import Navibar from "../../components/Navibar";
+import faker from "faker/locale/ko";
+faker.seed(100);
 function Company() {
+  // const columns = ["CompanyCode", "RshName", "Addr", "Phone"];
+  // const data = Array(15)
+  //   .fill()
+  //   .map(() => ({
+  //     CompanyCode: faker.random.number(),
+  //     RshName: faker.company.companyName(),
+  //     Addr: faker.address.city(),
+  //     Phone: faker.phone.phoneNumber(),
+  //   }));
   return (
     <>
       <div>
         <Navibar />
       </div>
-      <div className="parent">
-        <Sidebar className="child" />
-        <p style={{ flex: 7, marginTop: "6vh" }}>
-          회사
-        </p>
+      <div style={{ display: "flex", width: "100%" }}>
+        <Sidebar />
+        <div
+          style={{
+            marginTop: "6vh",
+            // marginLeft: "10px",
+            // marginRight: "10px",
+            width: "100%",
+            flex: "1",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              background: "#585858",
+              height: "100%",
+            }}
+          >
+            <div style={{ background: "#2E9AFE", flex: "1" }}>aa</div>
+            <div style={{ background: "#2E9A2E", flex: "1" }}>bb</div>
+          </div>
+        </div>
       </div>
     </>
   );
