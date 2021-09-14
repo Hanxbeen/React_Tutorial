9dbe9662c3eff8b9a87bcb446c3eba6ec51199ba
Hanxbeen <hanxbeen@gmail.com>
2021-09-14 16:51:55 +0900

    chore : 'babel 설치'

--- inflearn/hello-world/simple1.js
+++ inflearn/hello-world/simple1.js
@@ -1,19 +1,33 @@
 function LikeButton() {
-  const [liked, setLiked] = React.useState(false);
-  //useState함수는 컴포넌트에 상태값을 추가할 때 사용한다.
+  const [liked, setLiked] = React.useState(false); //useState함수는 컴포넌트에 상태값을 추가할 때 사용한다.
+
   const text = liked ? "좋아요 취소" : "좋아요";
-  return React.createElement(
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => setLiked(!liked)
+  }, text);
+  {
+    /*
+    return React.createElement(
     //react 요소를 반환한다
     //ui를 표현하는 가장 작은 단위가 react요소
     "button",
     { onClick: () => setLiked(!liked) },
     text
-  );
+    ); 
+    */
+  }
 }
 
 function CountBtn() {
   const [count, setCount] = React.useState(0);
-  return React.createElement(
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, count), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count + 1)
+  }, "\uC99D\uAC00"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count - 1)
+  }, "\uAC10\uC18C"));
+  {
+    /*
+    return React.createElement(
     "div",
     null,
     React.createElement("p", null, count),
@@ -27,7 +41,9 @@ function CountBtn() {
       { onClick: () => setCount(count - 1) },
       "감소"
     )
-  );
+    );
+    */
+  }
 }
 
 const domContainer = document.getElementById("root");
---
+++ inflearn/hello-world/src/simple1.js
@@ -0,0 +1,51 @@
+function LikeButton() {
+  const [liked, setLiked] = React.useState(false);
+  //useState함수는 컴포넌트에 상태값을 추가할 때 사용한다.
+  const text = liked ? "좋아요 취소" : "좋아요";
+  return <button onClick={() => setLiked(!liked)}>{text}</button>;
+  {
+    /*
+    return React.createElement(
+    //react 요소를 반환한다
+    //ui를 표현하는 가장 작은 단위가 react요소
+    "button",
+    { onClick: () => setLiked(!liked) },
+    text
+  ); 
+  */
+  }
+}
+
+function CountBtn() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      {/*<LikeButton />*/}
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>증가</button>
+      <button onClick={() => setCount(count - 1)}>감소</button>
+    </div>
+  );
+  {
+    /*
+   return React.createElement(
+    "div",
+    null,
+    React.createElement("p", null, count),
+    React.createElement(
+      "button",
+      { onClick: () => setCount(count + 1) },
+      "증가"
+    ),
+    React.createElement(
+      "button",
+      { onClick: () => setCount(count - 1) },
+      "감소"
+    )
+  );
+  */
+  }
+}
+
+const domContainer = document.getElementById("root");
+ReactDOM.render(React.createElement(CountBtn), domContainer);
