d43653ce1ee50012a8d2d217a840706c32953d7c
Hanxbeen <hanxbeen@gmail.com>
2021-09-14 16:57:57 +0900

    feat : 'babel로 컴파일'

--- inflearn/hello-world/simple1.js
+++ inflearn/hello-world/simple1.js
@@ -2,48 +2,36 @@ function LikeButton() {
   const [liked, setLiked] = React.useState(false); //useState함수는 컴포넌트에 상태값을 추가할 때 사용한다.
 
   const text = liked ? "좋아요 취소" : "좋아요";
-  return /*#__PURE__*/React.createElement("button", {
-    onClick: () => setLiked(!liked)
-  }, text);
-  {
-    /*
-    return React.createElement(
-    //react 요소를 반환한다
-    //ui를 표현하는 가장 작은 단위가 react요소
+  return /*#__PURE__*/ React.createElement(
     "button",
-    { onClick: () => setLiked(!liked) },
+    {
+      onClick: () => setLiked(!liked),
+    },
     text
-    ); 
-    */
-  }
+  );
 }
 
 function CountBtn() {
   const [count, setCount] = React.useState(0);
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, count), /*#__PURE__*/React.createElement("button", {
-    onClick: () => setCount(count + 1)
-  }, "\uC99D\uAC00"), /*#__PURE__*/React.createElement("button", {
-    onClick: () => setCount(count - 1)
-  }, "\uAC10\uC18C"));
-  {
-    /*
-    return React.createElement(
+  return /*#__PURE__*/ React.createElement(
     "div",
     null,
-    React.createElement("p", null, count),
-    React.createElement(
+    /*#__PURE__*/ React.createElement("p", null, count),
+    /*#__PURE__*/ React.createElement(
       "button",
-      { onClick: () => setCount(count + 1) },
-      "증가"
+      {
+        onClick: () => setCount(count + 1),
+      },
+      "\uC99D\uAC00"
     ),
-    React.createElement(
+    /*#__PURE__*/ React.createElement(
       "button",
-      { onClick: () => setCount(count - 1) },
-      "감소"
+      {
+        onClick: () => setCount(count - 1),
+      },
+      "\uAC10\uC18C"
     )
-    );
-    */
-  }
+  );
 }
 
 const domContainer = document.getElementById("root");
