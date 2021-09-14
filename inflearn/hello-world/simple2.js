12afd64fa2e084884576366580b14ffeb5fbdbfc
Hanxbeen <hanxbeen@gmail.com>
2021-09-14 14:43:58 +0900

    study : 'dom에 button과 div>p 태그 렌더링'

--- inflearn/hello-world/simple2.js
+++ inflearn/hello-world/simple2.js
@@ -1,6 +1,11 @@
 function helloText() {
   const text = "hello world";
-  return React.createElement("p", text);
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("p", null, "그냥 그렇다고.."),
+    React.createElement("p", null, text)
+  );
 }
 
 const domContainer2 = document.getElementById("root2");
